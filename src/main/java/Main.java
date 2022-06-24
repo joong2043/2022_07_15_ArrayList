6ba73895386f516a41461e9e86efbdb32fa157c6
joong2043 <tbvjqlqlqlr@gmail.com>
2022-06-24 15:50:07 +0900

    작업 1

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Car A = new Car();
+        Car B = new Car();
+
+        A.maxSpeed=60;
+        B.maxSpeed=50;
+
+    }
+}
+
+class Car{
+
+    // 동사
+    void run(){
+
+    };
+
+    int maxSpeed;
+}
